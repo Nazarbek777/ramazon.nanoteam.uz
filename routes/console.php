22102a718b9fb7db720928dc5619220f4f0140e3
nazarbekrashidov9373 <nazarbekrashidov9373@gmail.com>
2026-03-04 11:57:47 +0500

    fix

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Har kuni soat 08:00 da bazani bot foydalanuvchilariga yuborish
-Schedule::command('bot:send-daily-baza')->dailyAt('08:00');
+// Har kuni soat 08:00 da (UZT) bazani bot foydalanuvchilariga yuborish
+Schedule::command('bot:send-daily-baza')->dailyAt('08:00')->timezone('Asia/Tashkent');
